ea7102910dcb4f7c6b099c911eca85bc81e2fb87
jaid7146 <jaid.sstech@gmail.com>
2024-11-15 13:10:45 +0530

    Delete list_of_file.php

--- list_of_file.php
+++
@@ -1,9 +0,0 @@
-<?Php 
-$path="filess";
-$items=scandir($path);
-$items= array_diff($items,array(".",".."));
-foreach($items as $item){
-    echo "<a href=./filess/$item>$item</a>";
-    echo "<BR/>";
-}
-?>
