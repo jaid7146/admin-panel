83aa9e1198507fe4270cd47c260d90a1ab736559
jaid7146 <jaid.sstech@gmail.com>
2024-11-15 17:03:25 +0530

    delect normal titel

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Registration Form</title>
+    <title> Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
